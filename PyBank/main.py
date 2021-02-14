34ba3d4e4fd3ac86107d1e7104fbcfae8d4e011e
supriyada <supriyada@gmail.com>
2021-02-13 23:33:10 -0500

    Includes Bank financial analysis and write to terminal

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,7 +6,10 @@ import csv
 #Path to the data source file
 budget_data_path=os.path.join("Resources","budget_data.csv")
 
-month_list=[]
+month_list = []
+profit_loss_list = []
+
+budget = {}
 
 #Open the file budget_data
 with open(budget_data_path) as input_data:
@@ -16,13 +19,42 @@ with open(budget_data_path) as input_data:
 
     # Read the header row first
     csv_budget_header = next(csv_budget_read)
-    #--print(f"CSV Header: {csv_budget_header}")
+    print(f"CSV Header: {csv_budget_header}")
 
+    total_month = 0
+    net_total = 0
+    greatest_profit = 0
+    greatest_loss = 0
     # Read each row of data after the header
     for budget_row in csv_budget_read:
-        #The first row is split with delimiter '-' and the value in first position is added to the list
-        month=budget_row[0].split("-")
-        month_list.append(month[0])
+        
+        #Increment counter for each row read
+        total_month += 1
 
-    print("The number of months is: " + str(len(month_list)))
+        month_list.append(budget_row[0])
+        profit_loss_list.append(budget_row[1])
+
+        #Calculate the net total amount of "Profit/Losses" over the entire period
+        net_total += int(budget_row[1])
+
+        if int(budget_row[1]) > greatest_profit:
+            greatest_profit = int(budget_row[1])
+            profit_month = budget_row[0]
+
+        if int(budget_row[1]) < greatest_loss:
+            greatest_loss = int(budget_row[1])
+            loss_month = budget_row[0]
+        
+    budget = {"Month": month_list,"Amount": profit_loss_list}
+    initial_amount = int(budget["Amount"][0])
+    final_amount = int(budget["Amount"][total_month-1])
+    Change_profit_loss = round(((final_amount - initial_amount)/total_month),3)
+
+    print(f'Financial Analysis')
+    print(f'-'*30)
+    print("Total Months: " + str(total_month))
+    print("Total: " +str(net_total))
+    print("Average Change: "+str(Change_profit_loss))
+    print(f'Greatest Increase in Profits: {profit_month} (${str(greatest_profit)})')
+    print(f'Greatest Decrease in Profits: {loss_month} (${str(greatest_loss)})')
     
