069614de7fd2fd38de61666266fe26b1a95ff557
supriyada <supriyada@gmail.com>
2021-02-19 14:56:38 -0500

    Improvised the logic by using one Dictionary instead of multiple lists to store candidate results in PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,38 +3,22 @@ import os
 #import CSV module
 import csv
 
-candidates_list = []
-vote_count = []
-vote_percent = []
+"""
+Dictionary to store the candidate name and their total votes received.
+After creating dictionary, the values are used in calculating percentage of votes received and then find the winner!!!
+"""
+candidates_vote_dict = {}
+
 total_votes = 0
-percent_calc = 0.00
-
-#Calling function to create 2 lists: candidate list and their corresponding voting count
-def create_candidate_list(election_row):
-    if election_row[2] not in candidates_list:
-            candidates_list.append(election_row[2])
-            vote_count.append('1')
-        
-    else:
-            position = candidates_list.index(election_row[2])
-            vote_count[position] = int(vote_count[position]) + 1
-    return
-
-#Calling function to calculate %of vote received by each candidate and to lookup for winning candidate
-def calc_percent_vote_winner(vote_count):
-    winner = 0
-    for vote in range(len(vote_count)):
-        percent_calc = round((int(vote_count[vote])/total_votes)*100,2)
-        vote_percent.append(percent_calc)
-        if vote_count[vote] > winner:
-            winner = vote_count[vote]
-            winning_candidate = candidates_list[vote]
-    return winning_candidate
+winner = 0.00
 
 #Path to the data source file
 election_data_path=os.path.join("Resources","election_data.csv")
 
-#Open the file election_data
+#Path to the data output file
+output_path = os.path.join( "Analysis", "Results_from_Analysis.txt")
+
+#Open the file `election_data.csv` as input_data
 with open(election_data_path) as input_data:
 
     # CSV reader specifies delimiter and variable that holds contents
@@ -43,45 +27,48 @@ with open(election_data_path) as input_data:
     # Stores the header row
     csv_election_header = next(csv_election_read)
 
-    # Read each row of data after the header
     for election_row in csv_election_read:
 
         total_votes +=1
-        
-        #Calling function to create 2 lists: candidate list and their corresponding voting count
-        create_candidate_list(election_row)
 
-    #Calling function to calculate %of vote received by each candidate and to lookup for winning candidate       
-    winning_candidate =  calc_percent_vote_winner(vote_count)  
-   
-    #Print the Election data summary to the terminal
-    print("-"*30)
-    print(f'Election Results')
-    print("*"*16 + '\n')
-    print(f'Total Votes: {str(total_votes)}')
-    print("-"*30)
-    for vote in range(len(vote_count)):
-        print(candidates_list[vote] + ": " + str(vote_percent[vote]) + "%  (" +str(vote_count[vote]) + ")")
-    print("-"*30)
-    print("The Winner is: " + winning_candidate.title())
-    print("-"*30)
+        #Creating candidate entries and accumulating vote count in the dictionary    
+        if election_row[2] not in candidates_vote_dict:
+            candidates_vote_dict.update({election_row[2]:1})
+        else:
+            previous_vote_count =  candidates_vote_dict.get(election_row[2])
+            vote_count = int(previous_vote_count)+1
+            candidates_vote_dict.update({election_row[2]:vote_count})
 
-#Path to the data output file
-output_path = os.path.join( "Analysis", "Results_from_Analysis.txt")
+    # Open the output file in "write" mode.
+    with open(output_path, 'w', newline='') as csv_poll_write:
 
-# Open the file in "write" mode.
-with open(output_path, 'w', newline='') as csv_poll_write:
+        # Initialize csv.writer
+        csvwriter = csv.writer(csv_poll_write)
 
-    # Initialize csv.writer
-    csvwriter = csv.writer(csv_poll_write)
+        #Print the results to terminal and write it to a file
+        csvwriter.writerow(['Election Results'])
+        csvwriter.writerow(["*"*16])
+        csvwriter.writerow(["Total Votes: "+str(total_votes)])
+        csvwriter.writerow(["-"*30])
+        print("-"*30)
+        print(f'Election Results')
+        print("*"*16 + '\n')
+        print(f'Total Votes: {str(total_votes)}')
+        print("-"*30)
 
-    #Write Poll data Analysis summary output to the File    
-    csvwriter.writerow(['Election Results'])
-    csvwriter.writerow(["*"*16])
-    csvwriter.writerow(["Total Votes: "+str(total_votes)])
-    csvwriter.writerow(["-"*30])
-    for vote in range(len(vote_count)):
-        csvwriter.writerow([candidates_list[vote] + ": " + str(vote_percent[vote]) + "%  (" +str(vote_count[vote]) + ")"])
-    csvwriter.writerow(["-"*30])
-    csvwriter.writerow(["The Winner is: " + winning_candidate.title()])
-    csvwriter.writerow(["-"*30])
+        #Calculate the percentage of votes received and lookup for the winning candidate      
+        for key,value in candidates_vote_dict.items():
+            percent_calc = round((int(value)/total_votes)*100,2)
+            print(f'{key} : {str(percent_calc)}% ({str(value)})')
+            csvwriter.writerow([key + ":" + str(percent_calc) +  "% (" + str(value) + ")"])
+            
+            if percent_calc > winner:
+                winner = percent_calc
+                winning_candidate = key
+        print("-"*30)
+        print("The Winner is: " + winning_candidate.title())
+        print("-"*30)
+        csvwriter.writerow(["-"*30])
+        csvwriter.writerow(["The Winner is: " + winning_candidate.title()])
+        csvwriter.writerow(["-"*30]) 
+
